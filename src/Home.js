00428cfca0b58046f254272d623b17fab4db8df1
Syailendra <syailendramuhammad@gmail.com>
2021-07-30 13:59:31 +0700

    7 click event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Clicked',e)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Clicked by' + name, e.target)
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('marhio',e)}>Click me again</button>
     </div>
    );
 }
  
-export default Home;
+export default Home ;
