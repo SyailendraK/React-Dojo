372bdc05026df45db2f5427da6e4d0e873803b04
Syailendra <syailendramuhammad@gmail.com>
2021-07-31 10:59:12 +0700

    20 making custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (url) => {
+
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
+  
+  useEffect(() => {
+    fetch(url)
+    .then(res => {
+      if(!res.ok){
+        throw Error('could not fetch the data');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      setData(resData);
+      setIsPending(false);
+      setError(null);
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false);
+    });
+  }, [url]);
+
+  return {data, isPending, error};
+}
+
+export default useFetch;
