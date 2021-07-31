498dab041a6f4895ab26e86104add31a306a2c2b
Syailendra <syailendramuhammad@gmail.com>
2021-07-31 10:48:29 +0700

    19 fetch error handling

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,33 @@
+/* eslint-disable no-unused-expressions */
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  // let name = "ali";
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-    .then(res => res.json())
+    .then(res => {
+      if(!res.ok){
+        throw Error('could not fetch the data');
+      }
+      return res.json();
+    })
     .then(data => {
       setBlogs(data);
       setIsPending(false);
-    })
+      setError(null);
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false);
+    });
   }, []);
 
   return ( 
     <div className="home">
+    {error && <div>{error}</div>}
     {isPending && <div>Loading...</div>}
     {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
