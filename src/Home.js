f2c14d23e5a90c0b6044804709cc1e7ff72e6da0
Syailendra <syailendramuhammad@gmail.com>
2021-07-31 10:08:35 +0700

    18 conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
   // let name = "ali";
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
-
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    console.log('effect');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+    .then(res => res.json())
+    .then(data => {
+      setBlogs(data);
+      setIsPending(false);
+    })
+  }, []);
 
   return ( 
     <div className="home">
-    <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-    <button onClick={() => setName('luigi')}>chane name</button>
-    <p>{name}</p>
+    {isPending && <div>Loading...</div>}
+    {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
