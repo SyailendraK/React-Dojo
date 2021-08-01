58accd16be06cf9d5a439e0d5d7a295faca4f2ed
Syailendra <syailendramuhammad@gmail.com>
2021-08-01 09:17:44 +0700

    useEffect cleanUp

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,7 +7,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null)
   
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+
+    fetch(url, {signal: abortCont.signal})
     .then(res => {
       if(!res.ok){
         throw Error('could not fetch the data');
@@ -19,9 +21,13 @@ const useFetch = (url) => {
       setIsPending(false);
       setError(null);
     }).catch(err => {
+      if(err.name === 'AbortError'){
+        console.log('fetch aborted')
+      }
       setError(err.message)
       setIsPending(false);
     });
+    return () => abortCont.abort();
   }, [url]);
 
   return {data, isPending, error};
